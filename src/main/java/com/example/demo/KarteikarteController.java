35880e0e6dbf391b946263a56798e852adbce51c
Tobislz <tobsi2411@gmail.com>
2025-10-15 10:45:56 +0200

    MS1: Fertiger Controller und GetRoute

--- src/main/java/com/example/demo/KarteikarteController.java
+++ src/main/java/com/example/demo/KarteikarteController.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KarteikarteController {
+
+    @GetMapping("/karteikarten")
+    public String Hello() {
+        return "Hallo, willkommen im Karteikarten-System!";
+    }
 
 }
